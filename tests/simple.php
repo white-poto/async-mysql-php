b0daac0d716a0673ee3cb338432c95cb4ca78b85
Jenner <hypxm@qq.com>
2015-07-20 17:46:50 +0800

    update

--- tests/simple.php
+++ tests/simple.php
@@ -6,11 +6,11 @@
  * Time: 17:40
  */
 
-require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
+require dirname(dirname(__FILE__)) . '/src/Async.php';
 
 try{
     $async_mysql = new \Jenner\Mysql\Async();
+	var_dump($async_mysql);
     $async_mysql->attach(
         ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
         'select * from dd'
