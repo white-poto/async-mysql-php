1e730931a2f3924f36602bb3bae6994c722fb641
huyanping <hypxm@qq.com>
2015-07-20 17:40:02 +0800

    add Test

--- tests/AsyncTest.php
+++ tests/AsyncTest.php
@@ -9,25 +9,30 @@
 class AsyncTest extends PHPUnit_Framework_TestCase
 {
     public function testExecute(){
-        $async_mysql = new Async();
-        $async_mysql->attach(
-            ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
-            'select * from dd'
-        );
-        $async_mysql->attach(
-            ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
-            'select * from dd'
-        );
-        $async_mysql->attach(
-            ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
-            'select * from dd'
-        );
-        $async_mysql->attach(
-            ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
-            'select * from dd'
-        );
-        $result = $async_mysql->execute();
-        var_dump($result);
+        try{
+            $async_mysql = new Async();
+            $async_mysql->attach(
+                ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
+                'select * from dd'
+            );
+            $async_mysql->attach(
+                ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
+                'select * from dd'
+            );
+            $async_mysql->attach(
+                ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
+                'select * from dd'
+            );
+            $async_mysql->attach(
+                ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
+                'select * from dd'
+            );
+            $result = $async_mysql->execute();
+            var_dump($result);
+        }catch (Exception $e){
+            echo $e->getMessage();
+        }
+
     }
 
 }
