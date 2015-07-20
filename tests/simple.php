cd7a8e76f62677ef78ef902bc147f2bc16d64b83
huyanping <hypxm@qq.com>
2015-07-20 17:54:22 +0800

    add Test

--- tests/simple.php
+++ tests/simple.php
@@ -10,7 +10,6 @@ require dirname(dirname(__FILE__)) . '/src/Async.php';
 
 try{
     $async_mysql = new \Jenner\Mysql\Async();
-	var_dump($async_mysql);
     $async_mysql->attach(
         ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
         'select * from dd'
@@ -27,10 +26,7 @@ try{
         ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
         'select * from dd'
     );
-	var_dump($async_mysql);
     $result = $async_mysql->execute();
-    var_dump($result);
 }catch (Exception $e){
-var_dump($e);
     echo $e->getMessage();
 }
