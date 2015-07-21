ad5c948a76b149f7bd01b610703b84d8435d0575
huyanping <hypxm@qq.com>
2015-07-21 19:22:02 +0800

    add readme

---
+++ tests/async.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/7/21
+ * Time: 19:17
+ */
+
+echo microtime() . PHP_EOL;
+try{
+    $async_mysql = new \Jenner\Mysql\Async();
+    $async_mysql->attach(
+        ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
+        'select * from async'
+    );
+    $async_mysql->attach(
+        ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test'],
+        'select * from async'
+    );
+    $result = $async_mysql->execute();
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+
+echo microtime() . PHP_EOL;
+
+echo memory_get_usage() . PHP_EOL;
