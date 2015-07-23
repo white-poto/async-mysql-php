6d6f402c40e3541df3e570ff0e8620e20f16c624
huyanping <hypxm@qq.com>
2015-07-23 09:45:40 +0800

    add require-dev

---
+++ tests/performace_async.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/7/23
+ * Time: 9:37
+ */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$timer = new \Jenner\Timer();
+$timer->mark("start");
+for ($i = 0; $i < 10; $i++) {
+    try {
+        $async_mysql = new \Jenner\Mysql\Async();
+        $async_mysql->attach(
+            ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test', 'port' => 3306],
+            'select ID, NAME from async'
+        );
+        $async_mysql->attach(
+            ['host' => '127.0.0.1', 'user' => 'root', 'password' => '', 'database' => 'test', 'port' => 3306],
+            'select ID, NAME from async'
+        );
+        $result = $async_mysql->execute();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+$timer->mark("stop");
+
+$timer->printDiffReport();
---
+++ tests/performace_sync.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/7/23
+ * Time: 9:43
+ */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$timer = new \Jenner\Timer();
+$timer->mark('start');
+for ($i = 0; $i < 10; $i++) {
+    $mysql = mysqli_connect('127.0.0.1', 'root', '', 'test', '3306');
+    $result = $mysql->query("select * from async");
+    $result2 = $mysql->query("select * from async");
+}
+$timer->mark("stop");
+
+$timer->printDiffReport();
